bee54b79d57c4665508535382a0c740f115f8efc
Andrew Lane <andrew.lane@hobsons.com>
2014-02-27 14:54:57 -0500

    Adding the FriendInfoCard model

---
+++ Hackathon/Models/FriendInfoCard.cs
@@ -0,0 +1,38 @@
+﻿namespace Hackathon.Models
+{
+    /// <summary>
+    /// Models the information shows on the friend leaderboard screen
+    /// </summary>
+    public class FriendInfoCard
+    {
+        /// <summary>
+        /// Identifer for this friend in FB
+        /// </summary>
+        public string FriendId { get; set; }
+
+        /// <summary>
+        /// Path to an image that can be used to produce an <img /> tag for your friend's profile picture
+        /// </summary>
+        public string ProfilePictureImagePath { get; set; }
+
+        /// <summary>
+        /// Full name of your friend
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Metadata about the last social update (i.e. status update, picture posted) that this friend made
+        /// </summary>
+        public string LastSocialUpdateMetadata { get; set; }
+
+        /// <summary>
+        /// Numerical "physical" interaction score with this friend
+        /// </summary>
+        public int PhysicalInteractionScore { get; set; }
+
+        /// <summary>
+        /// Numerical "virtual" interaction score with this friend
+        /// </summary>
+        public int VirtualInteractionScore { get; set; }
+    }
+}
