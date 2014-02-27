9190414cb9aa337c61240734a6f0260e9552749e
Andrew Lane <andrew.lane@hobsons.com>
2014-02-27 18:42:21 -0500

    Invoking the real FacebookHelper to get the friends

--- Hackathon/Controllers/FriendsController.cs
+++ Hackathon/Controllers/FriendsController.cs
@@ -1,4 +1,5 @@
-﻿using Hackathon.Models;
+﻿using System.Linq;
+using Hackathon.Models;
 using System;
 using System.Collections.Generic;
 using System.Web.Http;
@@ -11,7 +12,10 @@ namespace Hackathon.Controllers
         // GET api/friends
         public IEnumerable<FriendInfoCard> Get()
         {
-            return GetAllDummyCards();
+            var fbHelper = new FaceBookHelper(AuthToken, UserId);
+
+            return
+                fbHelper.GetFriends().Select(friend => new FriendInfoCard { FriendId = friend.id, FullName = friend.name });
         }
 
         FriendInfoCard GetDummyCard(int index)
@@ -30,31 +34,6 @@ namespace Hackathon.Controllers
                 PhysicalInteractionScore = physicalScore,
                 VirtualInteractionScore = virtualScore,
                 ProfilePictureImagePath = "https://pbs.twimg.com/profile_images/2619899212/2zch7tkkmu327fkdaobt.jpeg"
-            };
-        }
-
-        IEnumerable<FriendInfoCard> GetAllDummyCards()
-        {
-            return new List<FriendInfoCard>
-            {
-                new FriendInfoCard
-                {
-                    FriendId = "olivianewtonjohn",
-                    FullName = "Olivia Newton-John",
-                    LastSocialUpdateMetadata = "Check out my new video: http://www.youtube.com/watch?v=vWz9VN40nCA",
-                    PhysicalInteractionScore = 6,
-                    VirtualInteractionScore = 5,
-                    ProfilePictureImagePath = "http://i.ytimg.com/vi/vWz9VN40nCA/0.jpg"
-                },
-                new FriendInfoCard
-                {
-                    FriendId = "helloworld",
-                    FullName = "Hello World",
-                    LastSocialUpdateMetadata = "Hackathon 2014",
-                    PhysicalInteractionScore = 2,
-                    VirtualInteractionScore = 8,
-                    ProfilePictureImagePath = "https://pbs.twimg.com/profile_images/2619899212/2zch7tkkmu327fkdaobt.jpeg"
-                }
             };
         }
 
