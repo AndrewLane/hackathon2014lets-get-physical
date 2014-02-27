3e4eff8e22ad66269ba76cab471fe21e752d0675
dankang <dartdanfm@gmail.com>
2014-02-27 13:39:16 -0500

    Basic angular scaffolding

---
+++ Hackathon/Client/init/app.js
@@ -0,0 +1,21 @@
+﻿"use strict";
+
+(function () {
+  var appModule = angular.module('hackathon', ['ui.router']);
+  appModule.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', routeConfig]);
+  function routeConfig($stateProvider, $urlRouterProvider, $locationProvider) {
+    $locationProvider.html5Mode(false).hashPrefix('');
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider.state('home', {
+      url: '/home',
+      templateUrl: '/Client/partials/home.html'
+    });
+
+    $stateProvider.state('test', {
+      url: '/test',
+      templateUrl: '/Client/partials/test.html',
+      controller: 'Test'
+    });
+  }
+}());
---
+++ Hackathon/Client/js/test.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+
+(function () {
+  angular.module('hackathon').controller('Test', ['$scope', '$state', '$stateParams', '$http', Test]);
+  function Test($scope, $state, $stateParams, $http) {
+    console.log("Test Controller");
+    $scope.testMessage = "Test Message";
+  }
+}());
