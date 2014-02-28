df9c6cd2939a3145870cbf516e3bdb10bd75ad57
dankang <dartdanfm@gmail.com>
2014-02-27 21:16:21 -0500

    leader board next button

--- Hackathon/Client/js/friendsctrl.js
+++ Hackathon/Client/js/friendsctrl.js
@@ -1,38 +1,33 @@
-angular.module( 'hackathon').controller( 'GetInfoCards', function ( $scope, $http, $stateParams, FacebookAuth )
-{
+angular.module('hackathon').controller('GetInfoCards', function ($scope, $http, $stateParams, FacebookAuth) {
   FacebookAuth.withAuth().then(function () {
     var fInd = $stateParams.index || '';
-	
-    if ((fInd === '') || (fInd === 0))
-    {
+    $scope.index = parseInt(fInd);
+
+    if ((fInd === '') || (fInd === 0)) {
       // get all
-      $http.get( '/api/friends/' ).success( function ( data )
-      {
+      $http.get('/api/friends/').success(function (data) {
         $scope.friendInfoCards = data;
-      } );
+      });
     }
-    else
-    {
+    else {
       // get specific
-      $http.get( '/api/friends/' + fInd ).success( function ( data )
-      {
-        $scope.friendInfoCards = data;
-      } );
-	
+      $http.get('/api/friends/' + fInd).then(function (response) {
+        $scope.friendInfoCards = response.data;
+      }, function () {
+        $state.transitionTo("/leaderboard", { index: 1 });
+      });
     }
   });
 });
 
-angular.module('hackathon').controller('NotifyFriend', function ($scope, $http, $stateParams) 
-{
-	var toAdd = $stateParams.toAdd || '';
-	var msg = $stateParams.toMsg || 'Hello there';
-	
-	$scope.notifyFriendSuccess = false;
-	
-	$http.get( '/api/notifyfriend/' + toAdd + '/' + msg ).success( function ( data )
-	{
-		$scope.notifyFriendSuccess = true;
-	} );
+angular.module('hackathon').controller('NotifyFriend', function ($scope, $http, $stateParams) {
+  var toAdd = $stateParams.toAdd || '';
+  var msg = $stateParams.toMsg || 'Hello there';
+
+  $scope.notifyFriendSuccess = false;
+
+  $http.get('/api/notifyfriend/' + toAdd + '/' + msg).success(function (data) {
+    $scope.notifyFriendSuccess = true;
+  });
 });
 
