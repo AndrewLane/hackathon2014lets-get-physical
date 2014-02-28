7f9e528f1ddb9c38925a571ca3d1e0603ad61dba
dankang <dartdanfm@gmail.com>
2014-02-27 20:12:34 -0500

    promise based auth

--- Hackathon/Client/init/fb.js
+++ Hackathon/Client/init/fb.js
@@ -1,52 +1,68 @@
-window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '582451755180049',
-      status     : true,
-      //cookie     : true,
-      xfbml      : true
-    });
+"use strict";
+(function () {
+  angular.module("hackathon").factory("FacebookAuth", ["Login", "$q", "$state", FacebookAuth]);
+  function FacebookAuth(Login, $q, $state) {
+    var authQ = $q.defer();
 
-    FB.Event.subscribe('auth.authResponseChange', function(response) {
+    window.fbAsyncInit = function () {
+      FB.init({
+        appId: '582451755180049',
+        status: true,
+        //cookie     : true,
+        xfbml: true
+      });
+
+      FB.Event.subscribe('auth.authResponseChange', function (response) {
         if (response.status === 'connected') {
-            testAPI();
+          testAPI();
         } else if (response.status === 'not_authorized') {
-            FB.login(function(response) {
-               // handle the response
-             });
+          authQ.reject("Not authorized");
+          authQ = $q.defer();
+          $state.transitionTo("/home");
+          FB.login(function (response) {
+            // handle the response
+          });
         } else {
-            FB.login(function(response) {
-               // handle the response
-             });
+          FB.login(function (response) {
+            // handle the response
+          });
         }
-    });
-};
+      });
+    };
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) { return; }
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/all.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 
-(function(d, s, id){
- var js, fjs = d.getElementsByTagName(s)[0];
- if (d.getElementById(id)) {return;}
- js = d.createElement(s); js.id = id;
- js.src = "//connect.facebook.net/en_US/all.js";
- fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
+    function testAPI() {
+      console.log('Welcome!  Fetching your information.... ');
+      FB.api('/me', function (response) {
         console.log('Good to see you, ' + response.name + '.');
-    });
-    FB.getLoginStatus(function(response) {
+      });
+      FB.getLoginStatus(function (response) {
         if (response.status === 'connected') {
 
-        var uid = response.authResponse.userID;
-        var accessToken = response.authResponse.accessToken;
-        FacebookLogin().SetAuth(uid, accessToken);
-        console.log('uid: ' + uid);
-        console.log('accessToken: ' + accessToken);
+          var uid = response.authResponse.userID;
+          var accessToken = response.authResponse.accessToken;
+          FacebookLogin().SetAuth(uid, accessToken);
+          authQ.resolve("Authorized!");
+          console.log('uid: ' + uid);
+          console.log('accessToken: ' + accessToken);
         } else if (response.status === 'not_authorized') {
-            // the user is logged in to Facebook,
-            // but has not authenticated your app
+          // the user is logged in to Facebook,
+          // but has not authenticated your app
         } else {
-            // the user isn't logged in to Facebook.
+          // the user isn't logged in to Facebook.
         }
-    });
-}
+      });
+    }
+    return {
+      withAuth: function () {
+        return authQ.promise;
+      },
+    };
+  }
+}());
